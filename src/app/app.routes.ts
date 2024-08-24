608c2db36900e553008dfe27135b5a0ceb511f8a
Harmary <maria.h4rlamova@gmail.com>
2024-08-25 00:09:33 +0300

    added ad page layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from '../pages/main-page/main-page.component';
+import { AdPageComponent } from '../pages/ad-page/ad-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     title: 'Main page',
     component: MainPageComponent
+  },
+  {
+    path: 'ad',
+    title: 'Ad page',
+    component: AdPageComponent
   }
 ];
---
+++ src/pages/ad-page/ad-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './ad-page.component.html',
+  styleUrl: './ad-page.component.scss'
+})
+export class AdPageComponent {
+
+}
